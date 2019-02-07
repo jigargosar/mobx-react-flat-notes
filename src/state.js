86f45ff77050277f9eb5a513938c812533718316
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-07 15:57:49 +0530

    init: app

--- src/state.js
+++ src/state.js
@@ -2,15 +2,16 @@ import { extendObservable, observable } from 'mobx'
 import { wrapActions } from './mobx-helpers'
 import nanoid from 'nanoid'
 import faker from 'faker'
+import { getCached } from './dom-helpers'
 
 function createState() {
-  const state = observable.object({ _noteList: [] }, null, {
-    name: 'Root State',
+  const state = observable.object({ noteList: [] }, null, {
+    name: 'App State',
   })
 
   return extendObservable(state, {
     get displayNotes() {
-      return state._noteList
+      return state.noteList
     },
   })
 }
@@ -19,12 +20,20 @@ const appState = createState()
 
 const appActions = wrapActions({
   add: () => {
-    appState._noteList.unshift({
+    appState.noteList.unshift({
       id: `N:${nanoid()}`,
       title: faker.name.lastName(null),
     })
   },
+  hydrate() {
+    const cached = getCached('app-state')
+    if (cached) {
+      appState.noteList = cached.noteList
+    }
+  },
 })
+
+appActions.hydrate()
 
 export function useAppState() {
   return appState

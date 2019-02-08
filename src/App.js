028e71bc33191baf5d3c072864b4a40de72e6292
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-08 16:50:05 +0530

    pouch sync settings dialog

--- src/App.js
+++ src/App.js
@@ -124,13 +124,13 @@ const App = observer(() => {
           <div className="flex-auto flex items-center">Flat Notes</div>
           <div className="flex items-center">
             <button
-              className="pointer ttu bg-inherit bn color-inherit"
+              className="pointer ttu bn bg-inherit color-inherit"
               onClick={actions.addNewNote}
             >
               <div className="underline-hover">{'Add'}</div>
             </button>
             <button
-              className="pointer ttu bg-inherit bn color-inherit"
+              className="pointer ttu bn bg-inherit color-inherit"
               onClick={actions.reset}
             >
               <div className="underline-hover">{'Reset'}</div>

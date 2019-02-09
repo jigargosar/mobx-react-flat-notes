2a9c875423fd4502e001506679321bb4a0965a04
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-09 19:00:34 +0530

    settings form

--- src/comp/PouchSettingsDialog.js
+++ src/comp/PouchSettingsDialog.js
@@ -3,6 +3,7 @@ import React, { useCallback, useImperativeHandle, useRef } from 'react'
 import { FocusTrapZone } from 'office-ui-fabric-react'
 import { useBoolObservable } from '../mobx/mobx-hooks'
 import { pd, useOnEsc } from '../hooks/keyboard'
+import * as R from 'ramda'
 
 const PouchSettingsDialog = observer(
   (_, ref) => {
@@ -21,7 +22,7 @@ const PouchSettingsDialog = observer(
     const onKeyDownHandler = useOnEsc(pd(open.not))
 
     return (
-      !open.get() && (
+      open.get() && (
         <FocusTrapZone>
           <div
             className="absolute absolute--fill flex items-center justify-center bg-black-50"
@@ -33,29 +34,35 @@ const PouchSettingsDialog = observer(
               className="relative w-80 mw6 bg-white shadow-1"
               // style={{ top: '-15%' }}
             >
-              <div className="pv4 ph3 f3 fw3 bg-blue white">
-                Pouch Sync Settings
-              </div>
-              <div className="ph3 ">
-                <label className="flex flex-column mv3">
-                  <div className="ml1 black-70 f6">URL</div>
-                  <input
-                    className="flex-auto pa2 "
-                    defaultValue={'http://a@b:localhost:2323'}
-                  />
-                </label>
-              </div>
-              <div className="ph3 pv2 bt b--light-gray flex flex-row-reverse">
-                <button className="pv2 ph3 ma0 link pointer bn bg-blue white">
-                  <div className="underline-hover">Save</div>
-                </button>
-                <button
-                  className="pv2 ph3 ma0 link pointer bn black-70"
-                  onClick={open.off}
-                >
-                  <div className="underline-hover">Cancel</div>
-                </button>
-              </div>
+              <form onSubmit={pd(R.identity)}>
+                <div className="pv4 ph3 f3 fw3 bg-blue white">
+                  Pouch Sync Settings
+                </div>
+                <div className="ph3 ">
+                  <label className="flex flex-column mv3">
+                    <div className="ml1 black-70 f6">URL</div>
+                    <input
+                      autoFocus
+                      className="flex-auto pa2 "
+                      defaultValue={'http://a@b:localhost:2323'}
+                    />
+                  </label>
+                </div>
+                <div className="ph3 pv2 bt b--light-gray flex flex-row-reverse">
+                  <button
+                    type="submit"
+                    className="pv2 ph3 ma0 link pointer bn bg-blue white"
+                  >
+                    <div className="underline-hover">Save</div>
+                  </button>
+                  <button
+                    className="pv2 ph3 ma0 link pointer bn black-70"
+                    onClick={open.off}
+                  >
+                    <div className="underline-hover">Cancel</div>
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </FocusTrapZone>

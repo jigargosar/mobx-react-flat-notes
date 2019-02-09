cec261e493e916b17830cd43916158342d658ff7
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-09 19:56:36 +0530

    settings form

---
+++ src/comp/Button.js
@@ -0,0 +1,35 @@
+import { observer } from 'mobx-react-lite'
+import React from 'react'
+
+export const FlatButton = observer(({ cls, label, ...p }) => {
+  cls = Object.assign(
+    {},
+    {
+      root: 'pv2 ph3 ma0 mh1 pointer bg-transparent ba b--transparent',
+      primary: 'bg-blue white ba b--white',
+      label: 'underline-hover',
+    },
+    cls,
+  )
+  return (
+    <button type="button" className={cls.root} {...p}>
+      <div className={cls.label}>{label}</div>
+    </button>
+  )
+})
+export const PrimaryButton = observer(({ cls, label, ...p }) => {
+  cls = Object.assign(
+    {},
+    {
+      root:
+        'pv2 ph3 ma0 mh1 pointer bg-transparent bg-blue white ba b--white',
+      label: 'underline-hover',
+    },
+    cls,
+  )
+  return (
+    <button type="button" className={cls.root} {...p}>
+      <div className={cls.label}>{label}</div>
+    </button>
+  )
+})
--- src/comp/PouchSettingsDialog.js
+++ src/comp/PouchSettingsDialog.js
@@ -5,40 +5,7 @@ import { useBoolObservable } from '../mobx/mobx-hooks'
 import { pd, useOnEsc } from '../hooks/keyboard'
 import * as R from 'ramda'
 import * as PropTypes from 'prop-types'
-
-const FlatButton = observer(({ cls, label, ...p }) => {
-  cls = Object.assign(
-    {},
-    {
-      root: 'pv2 ph3 ma0 mh1 pointer bg-transparent ba b--transparent',
-      primary: 'bg-blue white ba b--white',
-      label: 'underline-hover',
-    },
-    cls,
-  )
-  return (
-    <button type="button" className={cls.root} {...p}>
-      <div className={cls.label}>{label}</div>
-    </button>
-  )
-})
-
-const PrimaryButton = observer(({ cls, label, ...p }) => {
-  cls = Object.assign(
-    {},
-    {
-      root:
-        'pv2 ph3 ma0 mh1 pointer bg-transparent bg-blue white ba b--white',
-      label: 'underline-hover',
-    },
-    cls,
-  )
-  return (
-    <button type="button" className={cls.root} {...p}>
-      <div className={cls.label}>{label}</div>
-    </button>
-  )
-})
+import { FlatButton, PrimaryButton } from './Button'
 
 FlatButton.propTypes = {
   label: PropTypes.string.isRequired,

ad09d894b90bf96ca21063b29502683337ee81f5
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-10 15:01:58 +0530

    pouch sync url

--- src/comp/PouchSettingsDialog.js
+++ src/comp/PouchSettingsDialog.js
@@ -1,4 +1,4 @@
-import { observer, useDisposable } from 'mobx-react-lite'
+import { observer, useDisposable, useObserver } from 'mobx-react-lite'
 import React, { useCallback, useImperativeHandle, useRef } from 'react'
 import { FocusTrapZone } from 'office-ui-fabric-react'
 import { useBoolObservable, useStringObservable } from '../mobx/mobx-hooks'
@@ -40,47 +40,48 @@ const PouchSettingsDialog = observer(
 
     const onKeyDownHandler = useOnEsc(pd(open.not))
 
-    return (
-      open.get() && (
-        <FocusTrapZone>
-          <div
-            className="absolute absolute--fill flex items-center justify-center bg-black-50"
-            ref={backdropRef}
-            onClick={onBackdropClick}
-            onKeyDown={onKeyDownHandler}
-          >
+    return useObserver(
+      () =>
+        open.get() && (
+          <FocusTrapZone>
             <div
-              className="relative w-80 mw6 bg-white shadow-1"
-              // style={{ top: '-15%' }}
+              className="absolute absolute--fill flex items-center justify-center bg-black-50"
+              ref={backdropRef}
+              onClick={onBackdropClick}
+              onKeyDown={onKeyDownHandler}
             >
-              <form onSubmit={pd(onSubmitHandler)}>
-                <div className="pv4 ph3 f3 fw3 bg-blue white">
-                  Pouch Sync Settings
-                </div>
-                <div className="ph3 ">
-                  <label className="mv3 db">
-                    <div className="ml1 black-70 f6 lh-copy">
-                      Remote URL
-                    </div>
-                    <div className="flex flex-column  ba b--moon-gray ">
-                      <input
-                        autoFocus
-                        className="bn flex-auto pa2"
-                        placeholder="e.g: http://a@b:localhost:2323"
-                        {...pouchUrl.bindInput}
-                      />
-                    </div>
-                  </label>
-                </div>
-                <div className="pa3 bn b--light-gray flex flex-row-reverse">
-                  <PrimaryButton type="submit" label="Sync" />
-                  <FlatButton onClick={open.off} label="Cancel" />
-                </div>
-              </form>
+              <div
+                className="relative w-80 mw6 bg-white shadow-1"
+                // style={{ top: '-15%' }}
+              >
+                <form onSubmit={pd(onSubmitHandler)}>
+                  <div className="pv4 ph3 f3 fw3 bg-blue white">
+                    Pouch Sync Settings
+                  </div>
+                  <div className="ph3 ">
+                    <label className="mv3 db">
+                      <div className="ml1 black-70 f6 lh-copy">
+                        Remote URL
+                      </div>
+                      <div className="flex flex-column  ba b--moon-gray ">
+                        <input
+                          autoFocus
+                          className="bn flex-auto pa2"
+                          placeholder="e.g: http://a@b:localhost:2323"
+                          {...pouchUrl.bindInput}
+                        />
+                      </div>
+                    </label>
+                  </div>
+                  <div className="pa3 bn b--light-gray flex flex-row-reverse">
+                    <PrimaryButton type="submit" label="Sync" />
+                    <FlatButton onClick={open.off} label="Cancel" />
+                  </div>
+                </form>
+              </div>
             </div>
-          </div>
-        </FocusTrapZone>
-      )
+          </FocusTrapZone>
+        ),
     )
   },
   { forwardRef: true },

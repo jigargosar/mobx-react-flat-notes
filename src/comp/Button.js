f8e65f38dd1e14327410f52bc2c3a9e1bc32afb6
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-10 23:56:45 +0530

    monaco-helpers

--- src/comp/Button.js
+++ src/comp/Button.js
@@ -7,18 +7,6 @@ function mergeCls(def, cls) {
   return Object.assign({}, def, cls)
 }
 
-export const FlatButton = observer(function FlatButton({ ...p }) {
-  return (
-    <button
-      type="button"
-      className="pv2 ph3 ma0 mh1 pointer underline-hover bg-transparent ba b--transparent"
-      {...p}
-    />
-  )
-})
-
-FlatButton.displayName = 'FlatButton'
-
 const primaryButtonCls = {
   root:
     'pv2 ph3 ma0 mh1 pointer underline-hover bg-transparent bg-blue white ba b--white ',
@@ -39,22 +27,10 @@ PrimaryButton.propTypes = {
 
 PrimaryButton.displayName = 'PrimaryButton'
 
-// export const HeaderButton = observer(props => {
-//   return React.createElement(
-//     'button',
-//     R.mergeDeepRight(
-//       {
-//         type: 'button',
-//         className:
-//           'pv2 ttu f7 grow underline-hover  pointer bn bg-inherit color-inherit',
-//         children: '<no-label>',
-//       },
-//       props,
-//     ),
-//   )
-// })
-//
-// HeaderButton.displayName = 'HeaderButton'
+export const FlatButton = createButtonComponent(
+  'pv2 ph3 ma0 mh1 pointer underline-hover bg-transparent ba b--transparent',
+  'FlatButton',
+)
 
 export const HeaderButton = createButtonComponent(
   'pv2 ttu f7 grow underline-hover  pointer bn bg-inherit color-inherit',

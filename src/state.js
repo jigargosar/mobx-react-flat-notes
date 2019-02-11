da7d8c808b535b1da4b3e553f7f57c785c22e3b5
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-11 13:20:17 +0530

    pouch replication status

--- src/state.js
+++ src/state.js
@@ -198,38 +198,38 @@ async function setPouchUrlAndStartSync(newUrl) {
     window.remote = remote
   }
 
-  // multiEventStream(
-  //   ['change', 'paused', 'active', 'denied', 'complete', 'error'],
-  //   sync,
-  // ).each(console.log)
+  multiEventStream(
+    ['change', 'paused', 'active', 'denied', 'complete', 'error'],
+    sync,
+  ).each(console.log)
   if (process.env.NODE_ENV !== 'production') {
     window.sync = sync
   }
-  sync
-    .on('change', function(info) {
-      // handle change
-      console.log('change', info)
-    })
-    .on('paused', function(err) {
-      // replication paused (e.g. replication up to date, user went offline)
-      console.log('paused', err, this)
-    })
-    .on('active', function() {
-      // replicate resumed (e.g. new changes replicating, user went back online)
-      console.log('active')
-    })
-    .on('denied', function(err) {
-      // a document failed to replicate (e.g. due to permissions)
-      console.log('denied', err)
-    })
-    .on('complete', function(info) {
-      // handle complete
-      console.log('complete', info)
-    })
-    .on('error', function(err) {
-      // handle error
-      console.log('error', err)
-    })
+  // sync
+  //   .on('change', function(info) {
+  //     // handle change
+  //     console.log('change', info)
+  //   })
+  //   .on('paused', function(err) {
+  //     // replication paused (e.g. replication up to date, user went offline)
+  //     console.log('paused', err, this)
+  //   })
+  //   .on('active', function() {
+  //     // replicate resumed (e.g. new changes replicating, user went back online)
+  //     console.log('active')
+  //   })
+  //   .on('denied', function(err) {
+  //     // a document failed to replicate (e.g. due to permissions)
+  //     console.log('denied', err)
+  //   })
+  //   .on('complete', function(info) {
+  //     // handle complete
+  //     console.log('complete', info)
+  //   })
+  //   .on('error', function(err) {
+  //     // handle error
+  //     console.log('error', err)
+  //   })
 
   syncDisposable = function() {
     sync.cancel()

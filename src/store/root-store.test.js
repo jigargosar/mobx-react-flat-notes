479913746367190a9c0931bebd518143f135bbd5
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-11 20:29:10 +0530

    store refactoring, jest testing

--- src/store/root-store.test.js
+++ src/store/root-store.test.js
@@ -1,4 +1,5 @@
 import { NoteStore } from './root-store'
+import { getSnapshot } from 'mobx-state-tree'
 
 describe('NoteStore Smoke Tests', function() {
   test('NoteStore.create', async () => {
@@ -8,5 +9,6 @@ describe('NoteStore Smoke Tests', function() {
   test('NoteStore.addNote', async () => {
     const noteStore = NoteStore.create()
     noteStore.addNew()
+    console.log(`noteStore.map`, getSnapshot(noteStore.map))
   })
 })

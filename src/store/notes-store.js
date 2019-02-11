511b5eaa53bb3ff67c0d920ce9238cfa0daa36f8
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-11 18:42:42 +0530

    store refactoring

--- src/store/notes-store.js
+++ src/store/notes-store.js
@@ -1,6 +1,7 @@
 import { observable } from 'mobx'
 import nanoid from 'nanoid'
 import faker from 'faker'
+import { allDocsResultToDocs, notesDb } from '../pouch'
 
 class SubStore {
   rootStore
@@ -16,27 +17,40 @@ class Note extends SubStore {
   @observable title
   @observable content
 
-  constructor({ id, rev, title, content }, rootStore) {
+  constructor(rootStore, { id, rev, title, content }) {
     super(rootStore)
     Object.assign(this, { id, rev, title, content })
   }
 
   static create() {
-    return new Note({
+    return new Note(undefined, {
       id: `N:${nanoid()}`,
       rev: null,
       title: faker.name.lastName(null),
       content: faker.lorem.lines(),
     })
   }
+
+  static fromPouch(rootStore, { _id, _rev, title, content }) {
+    return new Note(rootStore, { id: _id, rev: _rev, title, content })
+  }
 }
 
 class NotesStore extends SubStore {
-  @observable list = []
+  @observable list = observable.array([])
 
   addNew() {
     const note = Note.create(this.rootStore)
     this.list.unshift(note)
+  }
+
+  async hydrate() {
+    const allDocsRes = await notesDb.allDocs({ include_docs: true })
+    const notes = allDocsResultToDocs(allDocsRes).map(
+      Note.fromPouch(rootStore),
+    )
+    console.debug(`[pouch] hydrating notes`, notes)
+    this.list.replace(notes)
   }
 }
 

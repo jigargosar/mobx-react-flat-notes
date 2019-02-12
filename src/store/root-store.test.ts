7a3bbe261283c5cbca70326d950992945671b89c
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-12 14:53:31 +0530

    store refactoring, jest testing

--- src/store/root-store.test.ts
+++ src/store/root-store.test.ts
@@ -2,29 +2,34 @@ import { newNote, Note, NoteStore } from './root-store'
 import { getSnapshot } from 'mobx-state-tree'
 import { createTestPouchDb } from './test-helper'
 
+async function withNotesDb(fn: Function) {
+  const notesDb = createTestPouchDb('notesDb')
+
+  await fn(notesDb)
+
+  await notesDb.destroy()
+}
+
 describe('NoteStore Smoke Tests', function() {
-  test('NoteStore.create', async () => {
+  test('NoteStore.create', () => {
     NoteStore.create()
   })
 
-  test('NoteStore.addNote', async () => {
-    const notesDb = createTestPouchDb('notesDb')
-
-    const noteStore = NoteStore.create({}, { notesDb })
-    noteStore.addNew()
-
-    await notesDb.destroy()
+  test('NoteStore.addNote', () => {
+    return withNotesDb((notesDb: PouchDB.Database) => {
+      const noteStore = NoteStore.create({}, { notesDb })
+      noteStore.addNew()
+    })
   })
+
   test('NoteStore.hydrate', async () => {
     const notesDb = createTestPouchDb('notesDb')
-
     await notesDb.put(Note.create(newNote()).asPouch)
 
     const noteStore = NoteStore.create({}, { notesDb })
     noteStore.addNew()
     await noteStore.hydrate()
     console.log(`noteStore.map`, getSnapshot(noteStore.map))
-
     await notesDb.destroy()
   })
 })

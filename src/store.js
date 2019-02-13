b40cbc374d1491b1cc4569f30d3eb2fee1efc829
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-13 13:11:47 +0530

    toast

--- src/store.js
+++ src/store.js
@@ -16,6 +16,43 @@ import { getCached, setCache } from './dom-helpers'
 import { NotesStore } from './store/NotesStore'
 import { ToastStore } from './store/ToastStore'
 
+function EditorStore({ id, rev, content }) {
+  const es = m.observable.object(
+    {
+      id,
+      rev,
+      content,
+      ...wrapActions({
+        setContent(content) {
+          es.content = content
+        },
+        async save() {
+          const note = s.ns.setContent(es.content, id)
+          const { rev } = await notesDb.put(note)
+          s.ns.setRev(rev, note.id)
+          es.rev = rev
+        },
+      }),
+    },
+    null,
+    { name: 'EditorStore' },
+  )
+  console.debug(`'created EditorStore'`, 'created EditorStore')
+  return es
+}
+
+function ViewStore() {
+  const vs = m.observable.object(
+    {
+      es: null,
+    },
+    null,
+    { name: 'ViewStore' },
+  )
+  console.debug(`'created ViewStore'`, 'created ViewStore')
+  return vs
+}
+
 function createState() {
   const s = m.observable.object(
     {
